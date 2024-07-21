2002caf2c9dc6e5deea1b49e76d77f995da19a31
azharuddintazapay <azharuddin@tazapay.com>
2024-07-21 23:02:34 +0530

    Completed client code

--- client/client.go
+++ client/client.go
@@ -70,9 +70,56 @@ func (ca *ClientAction) executeAction(action int) {
 		log.Println("Ticket is :")
 		prettyPrint(response)
 	case 2:
+		request := &ticket.ReceiptRequest{}
+		fmt.Print("Enter the ticket/receipt id: ")
+		fmt.Scanf("%s", &request.TicketID)
+		response, err := ca.c.GetReceipt(context.Background(), request)
+		if err != nil {
+			log.Printf("There was an error: %s\n", err.Error())
+			break
+		}
+		log.Println("Ticket is :")
+		prettyPrint(response)
 	case 3:
+		request := &ticket.SectionUserRequest{}
+		fmt.Print("Enter the section: ")
+		fmt.Scanf("%s", &request.Section)
+		response, err := ca.c.GetUserBySection(context.Background(), request)
+		if err != nil {
+			log.Printf("There was an error: %s\n", err.Error())
+			break
+		}
+		log.Println("Users :")
+		prettyPrint(response)
 	case 4:
+		request := &ticket.RemoveUserRequest{}
+		fmt.Print("Enter the email id: ")
+		fmt.Scanf("%s", &request.Email)
+		response, err := ca.c.RemoveUser(context.Background(), request)
+		if err != nil {
+			log.Printf("There was an error: %s\n", err.Error())
+			break
+		}
+		log.Println("Status :")
+		prettyPrint(response)
 	case 5:
+		request := &ticket.UpdateUserSeatRequest{}
+		fmt.Print("Enter the ticket id: ")
+		fmt.Scanf("%s", &request.TicketID)
+
+		fmt.Print("Enter the new section: ")
+		fmt.Scanf("%s", &request.Section)
+
+		fmt.Print("Enter the new seat: ")
+		fmt.Scanf("%d", &request.Seat)
+
+		response, err := ca.c.UpdateUserSeat(context.Background(), request)
+		if err != nil {
+			log.Printf("There was an error: %s\n", err.Error())
+			break
+		}
+		log.Println("Status :")
+		prettyPrint(response)
 	default:
 		log.Fatalln("invalid option")
 	}
